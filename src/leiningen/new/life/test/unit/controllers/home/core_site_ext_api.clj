2adf2bc30c5467c6d487cd98f1e1b518b309c132
garycrawford <bigdaddy@garycrawford.co.uk>
2015-04-27 08:00:44 +0100

    improving doc strings on tests

--- src/leiningen/new/life/test/unit/controllers/home/core_site_ext_api.clj
+++ src/leiningen/new/life/test/unit/controllers/home/core_site_ext_api.clj
@@ -16,31 +16,31 @@
 
 (def people [{:name "Anonomous User" :location "Timbuktu"}])
 
-(facts "for each call to index"
-    (fact "the response has a 200 status code"
-      (home) => (status? 200)
-      (provided
-        (get-people) => people))
-  
-    (fact "the response has a text/html content type"
-      (home) => (content-type? "text/html")
-      (provided
-        (get-people) => people))
-  
-    (fact "the response model is well formed"
-      (let [response (home)]
-        (get-in response [:body :model])) => (contains {:people people})
-      (provided
-        (get-people) => people))
-  
-    (fact "the correct view is returned for a first time visitor"
-      (let [response (home)]
-        (get-in response [:body :view :path])) => "templates/home/introduction.mustache"
-      (provided
-        (get-people) => []))
+(facts "the home function response map"
+  (fact "contains a 200 status code"
+    (home) => (status? 200)
+    (provided
+      (get-people) => people))
 
-    (fact "a view function is returned"
-      (let [response (home)]
-        (get-in response [:body :view :fn])) => fn?
-      (provided
-        (get-people) => people)))
+  (fact "contains a text/html content type"
+    (home) => (content-type? "text/html")
+    (provided
+      (get-people) => people))
+
+  (fact "contains a view model containing people list data for the view"
+    (let [response (home)]
+      (get-in response [:body :model])) => (contains {:people people})
+    (provided
+      (get-people) => people))
+
+  (fact "contains a path to the view template to be rendered"
+    (let [response (home)]
+      (get-in response [:body :view :path])) => "templates/home/introduction.mustache"
+    (provided
+      (get-people) => []))
+
+  (fact "contains a view function which renders the view with the view model data"
+    (let [response (home)]
+      (get-in response [:body :view :fn])) => fn?
+    (provided
+      (get-people) => people)))
